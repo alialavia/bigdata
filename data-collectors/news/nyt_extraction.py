4385ccf845c42c8b77e825179f5b750f84e5a0a1
kentsay <naturen@gmail.com>
2014-10-25 00:11:26 +0200

    add parsing section sports, technology, and politics
    Former-commit-id: 5d4592368a4852103f92d570a13264e707991b97
    Former-commit-id: bbe3daf8e279a7078c6d1bf083dc25b5a7a843e5
    Former-commit-id: f0417c4eb92e4e0a535b65c0de52a899c6f2ca22

--- data-collectors/news/nyt_extraction.py
+++ data-collectors/news/nyt_extraction.py
@@ -6,24 +6,46 @@ from time import sleep, strftime
 DATA_PATH = 'nyt_data/'
 
 def main():
-  news_count = 0
-  output_stream = file(get_new_filename("news-model-technology-"), "w")
+  tech_output_stream     = file(get_new_filename("news-model-technology-"), "w")
+  sport_output_stream    = file(get_new_filename("news-model-sports-"), "w")
+  politics_output_stream = file(get_new_filename("news-model-politics-"), "w")
   for filename in os.listdir(DATA_PATH):
     print filename
     f = open(DATA_PATH + filename, 'r')
     try:
       newsArray = json.loads(f.readline())
       for news in newsArray['results']:
+        '''
+        extract data for news data model:
+        1. Headlines
+        2. Keywords: extract if exists, otherwise extract from url
+        3. Date
+        4. Summary/Description/Abstract/Snippet
+        5. News source
+        '''
+        if news['title']:         headlines  = news['title'] 
+        if news['published_date']:date       = news['published_date'] 
+        if news['abstract']:      abstract   = news['abstract'] 
+        if news['source']:        source     = news['source'] 
+ 
         if (news['section'] and news['section'] == "Technology"):
-          if news['title']:         title      = news['title'] 
-          if news['published_date']:date       = news['published_date'] 
-          if news['abstract']:      abstract   = news['abstract'] 
-          if news['url']:           url_keywords = urlparse(news['url']).path.split('/')[4]
-          if news['source']:        source     = news['source'] 
-          output_stream.write(json.dumps({"title": title, "date": date, "abstract": abstract, "url_keywords": url_keywords, "source": source}, sort_keys=True))
-          output_stream.write("\n")
-          news_count += 1
-          print news_count
+          if urlparse(news['url']).netloc.find("blog") == -1:
+            if news['url']:           keywords = urlparse(news['url']).path.split('/')[5]
+          else:
+            if news['url']:           keywords = urlparse(news['url']).path.split('/')[4]
+          tech_output_stream.write(json.dumps({"headlines": headlines, "date": date, "abstract": abstract, "keywords": keywords, "source": source}, sort_keys=True))
+          tech_output_stream.write("\n")
+          print "Technology"
+        elif (news['section'] and news['section'] == "Sports"):
+          if news['url']:           keywords = urlparse(news['url']).path.split('/')[5]
+          sport_output_stream.write(json.dumps({"headlines": headlines, "date": date, "abstract": abstract, "keywords": keywords, "source": source}, sort_keys=True))
+          sport_output_stream.write("\n")
+          print "Sports"
+        elif (news['section'] and news['section'] == "politics"):
+          if news['url']:           keywords = urlparse(news['url']).path.split('/')[5]
+          politics_output_stream.write(json.dumps({"headlines": headlines, "date": date, "abstract": abstract, "keywords": keywords, "source": source}, sort_keys=True))
+          politics_output_stream.write("\n")
+          print "Politics"
 
     except (ValueError, KeyError, TypeError, IndexError):
       print "JSON Format error"
